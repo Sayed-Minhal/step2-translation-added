85ca7f3eca051b9236f29854961a7e2e48086d62
Sayed Minhal <sayedminhal@gmail.com>
2022-02-14 00:16:33 +0530

    Translation deps and configuration/resources added

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import './i18n';
+
+import { useTranslation } from 'react-i18next';
+
+function App() {
+  const { t } = useTranslation();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          { t('myKey') }
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
